c5bfb43bbb62cc84a40ef12f91146e1e11c52666
Alejandrodlk <ale_165851@hotmail.com>
2022-09-10 18:22:31 -0300

    formularios

--- app.js
+++ app.js
@@ -9,4 +9,12 @@ app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname,'views','home.html'))
 });
 
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'views','login.html'))
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'views','register.html'))
+});
+
 app.listen(port, () => console.log(`Server running on port http://localhost:${port}`))
