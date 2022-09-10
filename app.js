021fc2c0f61ee861ac6a114965c79c1c45bc7487
Alejandrodlk <ale_165851@hotmail.com>
2022-09-10 18:13:27 -0300

    agregando estilos done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const app = express();
-const port = 3030;
+const port = 3050;
 
 app.use(express.static(path.join(__dirname ,'public')));
 
